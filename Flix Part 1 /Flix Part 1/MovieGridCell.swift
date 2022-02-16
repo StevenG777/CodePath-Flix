a4f09d727f3da8f3eec12dc8fa22ffabbbee8964
StevenG666 <96842497+StevenG666@users.noreply.github.com>
2022-02-15 23:49:50 -0800

    Add files via upload

---
+++ Flix Part 1 /Flix Part 1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix Part 1
+//
+//  Created by Steven Guo on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
